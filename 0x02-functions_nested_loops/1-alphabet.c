ea4f8616f33b38319fe175f4a9930f34e84f9a9c
Sinyamela <sinyamela@gmail.com>
2023-02-22 09:12:01 -0800

    1alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet
+ * print_alphabet - Alphabet from a to z
  * Return: void
  */
 
@@ -11,9 +11,13 @@ void print_alphabet(void)
 
 	char c;
 	for (c = 'a'; c <= 'z'; c++)
+	
 	{
+	
 	_putchar (c);
+	
 	}
+	
 	_putchar('\n');
 
 }
