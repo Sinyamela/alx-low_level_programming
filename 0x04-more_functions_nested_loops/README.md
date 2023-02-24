9a1fecbf7a9a2f5812c35544b769cec569a7361a
Sinyamela <sinyamela@gmail.com>
2023-02-24 09:38:09 -0800

    0isupper

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+first 0x04-more functions nested loops readme file
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include “main.h”
+#include <unistd.h>
+
+/**
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On succes 1
+* On error, -1 is returned, and errno is set appropriately
+*/
+
+Int _putchar(char c)
+{
+	Return (write(1 ,&, 1));
+}
